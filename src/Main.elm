d84caa2527307184e32ba818d56156bf680e4b02
Matthew Heard <matt@mattheard.net>
2021-10-24 09:09:33 +0200

    Refactor and reformat

--- src/Main.elm
+++ src/Main.elm
@@ -1,67 +1,99 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, button, div, li, text, ul)
 import Html.Events exposing (onClick)
 import Json.Decode as Decode exposing (decodeString)
 import Json.Encode as Encode exposing (encode)
-
 import Ports
 
-type alias RetrievedState = Maybe String
+
+type alias RetrievedState =
+    Maybe String
+
 
 main : Program RetrievedState Model Msg
 main =
-  Browser.element
-    { init = init
-    , update = update
-    , view = view
-    , subscriptions = \_ -> Sub.none
-    }
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
 
-type Msg = Increment
-type alias Model = Int
+
+type Msg
+    = Increment
+
+
+type alias Model =
+    Int
+
 
 init : Maybe String -> ( Model, Cmd msg )
 init flags =
-  let
-    model =
-      case flags of
-        Just modelJson ->
-          decodeStoredModel modelJson
+    let
+        model =
+            case flags of
+                Just modelJson ->
+                    decodeStoredModel modelJson
 
-        Nothing ->
-          0
-  in
-  ( model, Cmd.none )
+                Nothing ->
+                    0
+    in
+    ( model, Cmd.none )
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Increment ->
-      let
-        newModel = model + 1
-      in
-      ( newModel 
-      , saveModel newModel 
-      )
+    case msg of
+        Increment ->
+            let
+                newModel =
+                    model + 1
+            in
+            ( newModel
+            , saveModel newModel
+            )
+
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
+    model
+        |> contents
+        |> List.map List.singleton
+        |> List.map (li [])
+        |> ul []
+
+
+contents : Model -> List (Html Msg)
+contents model =
+    [ renderFunds model
+    , renderIncrementButton
     ]
+
+
+renderFunds : Model -> Html Msg
+renderFunds model =
+    model |> String.fromInt |> text
+
+
+renderIncrementButton : Html Msg
+renderIncrementButton =
+    button [ onClick Increment ] [ text "+" ]
+
 
 saveModel : Int -> Cmd msg
 saveModel model =
-  encode 0 (Encode.int model)
-    |> Ports.storeModel
+    encode 0 (Encode.int model)
+        |> Ports.storeModel
+
 
 decodeStoredModel : String -> Model
 decodeStoredModel modelJson =
-  case decodeString Decode.int modelJson of
-    Ok model ->
-      model
-    Err _ ->
-      0
+    case decodeString Decode.int modelJson of
+        Ok model ->
+            model
+
+        Err _ ->
+            0
